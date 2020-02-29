9992b910bed68494737e2b2c6aafd8bdb18887b7
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2020-02-29 23:37:33 +0900

    Use setuptools instead of distutils.

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
-from distutils.core import setup
+from setuptools import setup
 
 
 def load_readme() -> str:
-    with open('README.md') as fin:
+    with open("README.md") as fin:
         return fin.read()
 
 
-setup(name='mong',
-      version='0.0.1',
-      description='Moby Name Generator in Python',
-      long_description=load_readme(),
-      long_description_content_type='text/markdown',
-      url='https://github.com/toshihikoyanase/mong',
-      author='Toshihiko Yanase',
-      author_email='toshihiko.yanase@gmail.com',
-      packages=['mong'],
-      package_data={'mong': ['moby_dict.json']})
+setup(
+    name="mong",
+    version="0.0.2",
+    description="Moby Name Generator in Python",
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/toshihikoyanase/mong",
+    author="Toshihiko Yanase",
+    author_email="toshihiko.yanase@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    packages=["mong"],
+    package_data={"mong": ["moby_dict.json"]},
+)
