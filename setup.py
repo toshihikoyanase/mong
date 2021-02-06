3c6e675b966c93543f3d28076a55bfbfa474e69b
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2021-02-06 10:07:47 +0900

    Bump up version to 0.0.3a2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def load_readme() -> str:
 
 setup(
     name="mong",
-    version="0.0.3a",
+    version="0.0.3a2",
     description="Moby Name Generator in Python",
     long_description=load_readme(),
     long_description_content_type="text/markdown",
